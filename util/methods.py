0edbf912014a0ab5655eeaf8f6e355948296b9a1
ultravadim <61945064+ultravadim@users.noreply.github.com>
2021-06-07 20:36:38 +0300

    Update methods.py

--- util/methods.py
+++ util/methods.py
@@ -5,5 +5,4 @@ class Method(object):
     r = requests.Session()
 
     def mail(self, guid=None, stand=None):
-        response_json = self.r.get(f'http://util.ao5.in/support/api/{stand}/getProductEmail/{guid}').json()
-        return response_json
+        return self.r.get(f'http://util.ao5.in/support/api/{stand}/getProductEmail/{guid}').json()
