e99f6589e3a4a97c1f48d315e5ca2099717db0a1
ultravadim <61945064+ultravadim@users.noreply.github.com>
2021-06-09 11:27:36 +0300

    Update methods.py

--- util/methods.py
+++ util/methods.py
@@ -7,11 +7,11 @@ class Method(object):
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def mail(self, guid=None, stand=None):
+    def mail(self, guid: str, stand: str):
         return self.r.get(f'{self.base_url}/support/api/{stand}/getProductEmail/{guid}').json()
 
-    def publish_info(self, guid=None, stand=None):
+    def publish_info(self, guid: str, stand: str):
         return self.r.post(f'{self.base_url}/support/api/{stand}/publishProductInfo/{guid}').json()
 
-    def reset_request_status(self, request_guid=None, stand=None):
+    def reset_request_status(self, request_guid: str, stand: str):
         return self.r.post(f'{self.base_url}/support/api/{stand}/resetRequestStatus/{request_guid}').json()
